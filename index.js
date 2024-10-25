5c5fcd810dea13ed41f5101a1a881a5f8b81bb2c
pahadiniraj <sharma12345niraj@gmail.com>
2024-10-25 12:33:30 +0545

    vercel

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./src/db/index.js";
-import { app } from "./app.js";
+import app from "./app.js";
 
-// Load environment variables from .env file
 dotenv.config({
   path: "./.env",
 });
 
-// Connect to MongoDB
 connectDB()
   .then(() => {
-    // Handle server errors
     app.on("error", (err) => {
       console.error("Server error:", err);
       throw err;
     });
 
-    // Define a root route
-    app.get("/", (req, res) => {
-      res.send("Hello, World! I'm Niraj!");
-    });
-
-    // Start the server
-    const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not set
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
     console.error("Database connection failed:", err);
-    process.exit(1); // Exit the process if the database connection fails
+    process.exit(1);
   });
-
-export default app;
